c1625483fc6f98f0607b86adb9792376d386d17e
alice20240103 <alice20240103@gamil.com>
2025-04-03 09:33:17 +0900

    feat:도서관리 완료

--- madang_a/src/madang/vo/BookVO.java
+++ madang_a/src/madang/vo/BookVO.java
@@ -1,5 +1,44 @@
 package madang.vo;
 
 public class BookVO {
-
+	private int boookid;
+	private String bookname;
+	private int price;
+	private String publisher;
+	public BookVO(int boookid, String bookname, int price, String publisher) {
+		super();
+		this.boookid = boookid;
+		this.bookname = bookname;
+		this.price = price;
+		this.publisher = publisher;
+	}
+	public BookVO() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public int getBoookid() {
+		return boookid;
+	}
+	public void setBoookid(int boookid) {
+		this.boookid = boookid;
+	}
+	public String getBookname() {
+		return bookname;
+	}
+	public void setBookname(String bookname) {
+		this.bookname = bookname;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public String getPublisher() {
+		return publisher;
+	}
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
+	}
+	
 }
